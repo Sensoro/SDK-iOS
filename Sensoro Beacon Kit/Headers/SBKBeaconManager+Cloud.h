f72fd2bee8ba7d10acb137b3b2f7efa571b337a8
David Yang <yangzhiqiang@sensoro.com>
2015-03-05 13:47:08 +0800

    添加CloudSDK

---
+++ Sensoro Beacon Kit/Headers/SBKBeaconManager+Cloud.h
@@ -0,0 +1,15 @@
+//
+//  SBKBeaconManager.h
+//  SensoroCloud
+//
+//  Created by David Yang on 14/12/22.
+//  Copyright (c) 2014年 Sensoro. All rights reserved.
+//
+
+#import "SBKBeaconManager.h"
+
+@interface SBKBeaconManager (Cloud)
+
+- (void)setCloudServiceEnable:(BOOL)set;
+
+@end
